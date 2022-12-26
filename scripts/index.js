6fd04fa6ce0b217168c172333136a72cdd34be5a
nicasiof <nicasio@yahoo.com>
2022-12-26 18:22:17 -0300

    Initial commit

---
+++ scripts/index.js
@@ -0,0 +1,76 @@
+console.log('bienvenido');
+
+centesimas = 0;
+segundos = 0;
+minutos = 0;
+horas = 0;
+
+function startTime() {
+    var today = new Date();
+    var hr = today.getHours();
+    var min = today.getMinutes();
+    var sec = today.getSeconds();
+    //Add a zero in front of numbers<10
+    min = checkTime(min);
+    sec = checkTime(sec);
+    document.getElementById("clock").innerHTML = hr + " : " + min + " : " + sec;
+    var time = setTimeout(function () { startTime() }, 500);
+}
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
+function date() {
+    var today = new Date();
+    var months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'May', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+    var days = ['Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab'];
+    var curWeekDay = days[today.getDay()];
+    var curDay = today.getDate();
+    var curMonth = months[today.getMonth()];
+    var curYear = today.getFullYear();
+    var date = curWeekDay + ", " + curDay + " " + curMonth + " " + curYear;
+    document.getElementById("date").innerHTML = date;
+}
+
+function startTime2() {
+    control = setInterval(cronometro, 10);
+
+
+}
+
+function cronometro() {
+
+    if (centesimas < 99) {
+        centesimas++;
+        if (centesimas < 10) { centesimas = "0" + centesimas }
+        Centesimas.innerHTML = "Centesimas:" + centesimas;
+    }
+    if (centesimas == 99) {
+        centesimas = -1;
+    }
+    if (centesimas == 0) {
+        segundos++;
+        if (segundos < 10) { segundos = "0" + segundos }
+        Segundos.innerHTML = "Segundos:" + segundos;
+    }
+    if (segundos == 59) {
+        segundos = -1;
+    }
+    if ((centesimas == 0) && (segundos == 0)) {
+        minutos++;
+        if (minutos < 10) { minutos = "0" + minutos }
+        Minutos.innerHTML = "Minutos:" + minutos;
+    }
+    if (minutos == 59) {
+        minutos = -1;
+    }
+    if ((centesimas == 0) && (segundos == 0) && (minutos == 0)) {
+        horas++;
+        if (horas < 10) { horas = "0" + horas }
+        Horas.innerHTML = "Horas:" + horas;
+    }
+}
+
